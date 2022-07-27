5fc231a127fadfc2fde145e0d8d2c38df9708aa9
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2022-07-27 10:05:06 +0200

    Rotate.cs

--- Assets/Scripts/Rotate.cs
+++ Assets/Scripts/Rotate.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class Rotate : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private Vector3 rot;
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(rot * Time.deltaTime);
     }
 }
